527f3e9f2eb49880b84c38bc44afd1e9b7124e3e
dau <tarigasdau22@gmail.com>
2025-06-08 20:46:16 +0700

    Add files via upload

---
+++ PWD_latihan3.cpp
@@ -0,0 +1,44 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <title>Latihan 1 - HTML5</title>
+</head>
+<body>
+    <header>
+         <h2>Nabiel Keanu Tarigas Dau</h1>
+          <p>Web developer| Design | </p>
+</header>
+    <header>
+        <h2>Tentang Saya</h1>
+        <p>Saya adalah seorang pengembang web yang berfokus pada teknologi modern </p>
+</header>
+    <header>
+        <h2>Pendidikan</h1>
+        <ul>
+        <li>SMA - SMAN 1 Sungai Ambawang(2020-2023)
+</ul>
+</header>
+    <header>
+        <h2>Pengalaman Kerja</h1>
+        <ul>
+        <li>Belum memiliki Pengalaman Kerja
+</ul>
+</header>
+    <header>
+        <h2>Keahlian</h1>
+        <ul>
+        <li>Canva
+        <li>C++
+        <li>Figma
+</ul>
+</header>
+    <header>
+        <h2>Kontak</h1>
+        <p>Email    : nabielkeanutarigasdau@itbss.ac.id</p>
+        <p>Linkin   : Nabiel Keanu Tarigas Dau</p>
+        <p>Github   : nbiel22</p>
+        </header>
+
+        <a href="portofolio">Portofolio</a>
